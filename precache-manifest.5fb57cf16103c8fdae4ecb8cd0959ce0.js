ffad5fdfc24109a311c259ff1be98b6f5414798f
[create-com] <[mess.t6@gmail.com]>
2020-07-10 19:06:12 -0500

    Updates

---
+++ precache-manifest.5fb57cf16103c8fdae4ecb8cd0959ce0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff796e79de062f3bc67af32b44ed794",
+    "url": "/apple-live/index.html"
+  },
+  {
+    "revision": "e2d143c4ccb364d07419",
+    "url": "/apple-live/static/css/main.cd19ba8e.chunk.css"
+  },
+  {
+    "revision": "f2621164c568af726d5e",
+    "url": "/apple-live/static/js/2.44f227f3.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/apple-live/static/js/2.44f227f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d143c4ccb364d07419",
+    "url": "/apple-live/static/js/main.4cc77888.chunk.js"
+  },
+  {
+    "revision": "73ac15bc8c5a6fe8014a",
+    "url": "/apple-live/static/js/runtime-main.f7106e14.js"
+  },
+  {
+    "revision": "1cd845d6f5a1445d85db5f4f31baf865",
+    "url": "/apple-live/static/media/Arcon-lg.1cd845d6.jpg"
+  },
+  {
+    "revision": "6876fc6527efcaa8ae1386693dce73d5",
+    "url": "/apple-live/static/media/Card_large.6876fc65.jpg"
+  },
+  {
+    "revision": "45b2503e8bb62934357a42699c1f33fc",
+    "url": "/apple-live/static/media/Card_small.45b2503e.jpg"
+  },
+  {
+    "revision": "85ec05ea25a9fb0411b9823b8d29550a",
+    "url": "/apple-live/static/media/actors.85ec05ea.jpg"
+  },
+  {
+    "revision": "d01eca6e3cc318fa6465d047837e3da6",
+    "url": "/apple-live/static/media/apple-card-monthly.d01eca6e.jpg"
+  },
+  {
+    "revision": "5465cc591da2ac562ae16bb33f3575b8",
+    "url": "/apple-live/static/media/apple.5465cc59.svg"
+  },
+  {
+    "revision": "aea2d7189d99437b77d399f64935c8f3",
+    "url": "/apple-live/static/media/bag.aea2d718.svg"
+  },
+  {
+    "revision": "d34f65aa590c8d3a0928fb07f2d1289b",
+    "url": "/apple-live/static/media/central_park_lg.d34f65aa.jpg"
+  },
+  {
+    "revision": "edf94f2ff598948e0d9fdb0611b2cf0c",
+    "url": "/apple-live/static/media/central_park_sm.edf94f2f.jpg"
+  },
+  {
+    "revision": "a7ef5f4882048c96dcefc8239b534c8e",
+    "url": "/apple-live/static/media/iphone11-colored.a7ef5f48.jpg"
+  },
+  {
+    "revision": "573a6b368484e06a1543fd0af4d70bfa",
+    "url": "/apple-live/static/media/iphone11-pro-bg.573a6b36.jpg"
+  },
+  {
+    "revision": "30b06e91d5c36c592794c16ca7e42ae7",
+    "url": "/apple-live/static/media/ipodPronew-sm.30b06e91.jpg"
+  },
+  {
+    "revision": "f7f2ab1f61fd401300c25a2df3be6893",
+    "url": "/apple-live/static/media/ipodPronew.f7f2ab1f.jpg"
+  },
+  {
+    "revision": "851443b3e3a4a5ea65c4e33622cc4c42",
+    "url": "/apple-live/static/media/macbookair-new-sm.851443b3.jpg"
+  },
+  {
+    "revision": "890634a4409ae22865fbad51d6004953",
+    "url": "/apple-live/static/media/macbookair-new.890634a4.jpg"
+  },
+  {
+    "revision": "8dd1210f9d7be403307a214472c87f03",
+    "url": "/apple-live/static/media/search.8dd1210f.svg"
+  },
+  {
+    "revision": "3dda7621799aa621c3d6df229700440e",
+    "url": "/apple-live/static/media/spyder.3dda7621.jpg"
+  },
+  {
+    "revision": "aa3dc7331ab40ebb0b984342d3edb186",
+    "url": "/apple-live/static/media/watch.aa3dc733.jpg"
+  }
+]);
